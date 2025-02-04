dd6a40cd936e0154c6d63bd23aa6ab801766e3fd
Harsh <harshwardhandatascientist@gmail.com>
2025-02-04 17:02:47 +0530

    Update cloneRepo.groovy
    b

--- vars/cloneRepo.groovy
+++ vars/cloneRepo.groovy
@@ -1,8 +1,13 @@
-// Define the 'cloneRepo' method in your shared library
 def call(String repoUrl) {
     def config = loadConfig()  // Load config file
     def branch = config.ENVIRONMENT == "prod" ? "main" : "develop"
+    
+    // Directory name (assumes the repo is named shared_library)
+    def dirName = 'shared_library'
+    
+    // Remove the existing directory if it exists
+    sh "rm -rf ${dirName}"
 
     // Clone the repository based on the branch
-    sh "git clone -b ${branch} ${repoUrl}"
+    sh "git clone -b ${branch} ${repoUrl} ${dirName}"
 }
