9162215cf8055464333de49d90045142ee71d3f6
Harsh <harshwardhandatascientist@gmail.com>
2025-02-04 16:45:58 +0530

    Update cloneRepo.groovy
    n

--- vars/cloneRepo.groovy
+++ vars/cloneRepo.groovy
@@ -1,16 +1,8 @@
+// Define the 'cloneRepo' method in your shared library
 def call(String repoUrl) {
     def config = loadConfig()  // Load config file
     def branch = config.ENVIRONMENT == "prod" ? "main" : "develop"
 
-    pipeline {
-        stages {
-            stage('Clone Repository') {
-                steps {
-                    script {
-                        sh "git clone -b ${branch} ${repoUrl}"
-                    }
-                }
-            }
-        }
-    }
+    // Clone the repository based on the branch
+    sh "git clone -b ${branch} ${repoUrl}"
 }
